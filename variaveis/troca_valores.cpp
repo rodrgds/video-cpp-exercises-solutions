85084a2c854a3c1feedf2b40a6203f4baa1cb180
Kraktoos <kraktoos@gmail.com>
2025-03-11 10:10:43 +0000

    add gitignore

--- variaveis/troca_valores.cpp
+++ variaveis/troca_valores.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
 int main() {
-  int a = 5, b = 3;
+  int a, b;
   int temp;
+
+  std::cout << "Introduza o valor de A: "; std::cin >> a;
+  std::cout << "Introduza o valor de B: "; std::cin >> b;
+
+  std::cout << "Antes da troca: A = " << a << ", B = " << b << "\n";
 
   temp = a;
   a = b;
   b = temp;
+
+  std::cout << "Depois da troca: A = " << a << ", B = " << b << "\n";
 
   return 0;
 }
